54dc0846633da7637e5f63ade032d3f20a19f510
Supachai S. <pxsstx@gmail.com>
2025-01-11 00:53:26 +0700

    New Blog CPE270

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,7 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = [
-	'CPE101',
-	'CPE161',
-	'CPE263',
-	'CPE241'
-] as const
+export const CATEGORIES = ['CPE101', 'CPE161', 'CPE263', 'CPE241', 'CPE270'] as const
