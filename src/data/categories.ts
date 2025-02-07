03bfe17d134f0d934bf7b05628e7491c064bada9
Supachai S. <pxsstx@gmail.com>
2025-02-07 23:10:20 +0700

    updata astro add CPE338 week1-5 edit & add CPE270 edit week2 & add week3-5

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = ['CPE101', 'CPE161', 'CPE263', 'CPE241', 'CPE270'] as const
+export const CATEGORIES = ['CPE101', 'CPE161', 'CPE263', 'CPE241', 'CPE270', 'CPE338'] as const
