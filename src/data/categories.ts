6a0065f8b35e87f6d302ea2015565c97748d2bcf
Supachai S. <pxsstx@gmail.com>
2025-02-19 02:13:12 +0700

    add CPE360

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,10 @@
 // List of categories for blog posts
-export const CATEGORIES = ['CPE101', 'CPE161', 'CPE263', 'CPE241', 'CPE270', 'CPE338'] as const
+export const CATEGORIES = [
+	'CPE101',
+	'CPE161',
+	'CPE263',
+	'CPE241',
+	'CPE270',
+	'CPE338',
+	'CPE360'
+] as const
